20e75593dbe01368b6119d252c5a74698ec5a739
Cristian <cristian.jimenezsa@hotmail.com>
2024-10-14 16:11:44 -0500

    delete button tooltip

--- src/plugins/form/admin/src/components/controls/DeleteDialog.tsx
+++ src/plugins/form/admin/src/components/controls/DeleteDialog.tsx
@@ -1,5 +1,7 @@
 import { Button, Dialog, IconButton } from '@strapi/design-system';
 import { Trash, WarningCircle } from '@strapi/icons';
+import * as Tooltip from '@radix-ui/react-tooltip';
+import TooltipBox from '../ui/Tooltip';
 
 export const DeleteDialog = ({
   documentId,
@@ -8,33 +10,44 @@ export const DeleteDialog = ({
   documentId: string;
   confirmAction: (id: string) => void;
 }) => {
-  const { Root, Trigger, Content, Header, Body, Footer, Cancel, Action } = Dialog;
-
   return (
-    <Root>
-      <Trigger>
-        <IconButton variant="danger-light" withTooltip={false}>
-          <Trash />
-        </IconButton>
-      </Trigger>
-      <Content>
-        <Header>Confirmation</Header>
-        <Body icon={<WarningCircle fill="danger600" />}>
-          Are you sure you want to delete this entry?
-        </Body>
-        <Footer>
-          <Cancel>
-            <Button fullWidth variant="tertiary">
-              Cancel
-            </Button>
-          </Cancel>
-          <Action>
-            <Button fullWidth variant="danger-light" onClick={() => confirmAction(documentId)}>
-              Confirm
-            </Button>
-          </Action>
-        </Footer>
-      </Content>
-    </Root>
+    <Tooltip.Provider delayDuration={100} disableHoverableContent>
+      <Tooltip.Root>
+        <Tooltip.Trigger>
+          <Dialog.Root>
+            <Dialog.Trigger>
+              <IconButton variant="danger-light" withTooltip={false}>
+                <Trash />
+              </IconButton>
+            </Dialog.Trigger>
+            <Dialog.Content>
+              <Dialog.Header>Confirmation</Dialog.Header>
+              <Dialog.Body icon={<WarningCircle fill="danger600" />}>
+                Are you sure you want to delete this entry?
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Dialog.Cancel>
+                  <Button fullWidth variant="tertiary">
+                    Cancel
+                  </Button>
+                </Dialog.Cancel>
+                <Dialog.Action>
+                  <Button
+                    fullWidth
+                    variant="danger-light"
+                    onClick={() => confirmAction(documentId)}
+                  >
+                    Confirm
+                  </Button>
+                </Dialog.Action>
+              </Dialog.Footer>
+            </Dialog.Content>
+          </Dialog.Root>
+        </Tooltip.Trigger>
+        <Tooltip.Content sideOffset={5}>
+          <TooltipBox tooltip="Delete" />
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   );
 };
